7de101feca4e31889ff4c4c8d2adce4f83a35053
SkyNotion <meshdrew5@gmail.com>
2025-08-15 12:09:30 +0100

    Added (and Registered in Kernel.php) user authentication middleware for authenticating with email and password

---
+++ app/Http/Middleware/UserAuthMiddleware.php
@@ -0,0 +1,55 @@
+<?php namespace App\Http\Middleware;
+
+use Closure;
+use Validator;
+use Hash;
+
+use App\User;
+
+class UserAuthMiddleware {
+
+	public $validation_messages = [
+		'email.required' => 'Email is missing',
+		'email.email' => 'Email not valid',
+		'password.required' => 'Password is missing'
+	];
+
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{	
+		$params = $request->all();
+		$rv = Validator::make($params,
+			['email' => 'required|email',
+			 'password' => 'required'],
+			 $this->validation_messages
+		);
+
+		if($rv->fails()){
+			return response()->json(['error' => $rv->errors()->first()], 400);
+		}
+
+		$unauthorized = response()->json(
+		['error' => 'Unauthorized, invalid email or password'], 401);
+
+		$user = User::where('email', '=', $params['email'])->first();
+
+		if(sizeof($user)){
+			if(!Hash::check($params['password'], $user['password'])){
+				return $unauthorized;
+			}
+		}else{
+			return $unauthorized;
+		}
+
+		$request->user_id = $user['user_id'];
+
+		return $next($request);
+	}
+
+}
