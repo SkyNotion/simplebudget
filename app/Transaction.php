3a698205f07c8169fba1cb25616c0621d7db3e5e
SkyNotion <meshdrew5@gmail.com>
2025-08-15 20:58:53 +0100

    Set account_id to be hidden when returning model instance array

--- app/Transaction.php
+++ app/Transaction.php
@@ -4,5 +4,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model {
 	protected $table = 'transactions';
+	protected $hidden = ['account_id'];
 	protected $primaryKey = 'transaction_id';
 }
