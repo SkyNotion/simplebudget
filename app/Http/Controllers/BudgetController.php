066bc251a0d6e1551cd668b972e81e9060d356bb
SkyNotion <meshdrew5@gmail.com>
2025-08-16 04:09:32 +0100

    Added BudgetController to handle routes to budget endpoint

---
+++ app/Http/Controllers/BudgetController.php
@@ -0,0 +1,67 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+use App\Budget;
+use App\Account;
+
+class BudgetController extends Controller {
+
+	public function index(Request $request, $account_id = null)
+	{
+		$account = Account::whereRaw('user_id = ? and account_id = ?',
+					[$request->user_id, $account_id])->first();
+		if(!sizeof($account)){
+			return response()->json(['error' => 'Account does not exist'], 404);
+		}
+		return response()->json(Budget::where('account_id', $account_id)->first()->toArray(), 200);
+	}
+
+	public function create(Request $request, $account_id = null)
+	{
+		$account = Account::whereRaw('user_id = ? and account_id = ?',
+					[$request->user_id, $account_id])->first();
+		if(!sizeof($account)){
+			return response()->json(['error' => 'Account does not exist'], 404);
+		}
+
+		$body = $request->all();
+		if(!sizeof($body)){
+			return response()->json(['error' => 'Request body cannot be empty'], 400);
+		}
+
+		$budget = Budget::firstOrNew(['account_id' => $account_id]);
+
+		if(isset($body['name'])){
+			$budget->name = $body['name'];
+		}
+		if(isset($body['description'])){
+			$budget->description = $body['description'];
+		}
+		if(isset($body['entities'])){
+			$budget_limit = 0.0;
+			for($i = 0;$i < sizeof($body['entities']);$i++){
+				$budget_limit += $body['entities'][$i]['amount'];
+			}
+			$budget->entities = json_encode($body['entities']);
+			$budget->budget_limit = $budget_limit;
+		}	
+		$budget->save();
+		return response()->json($budget->fresh()->toArray(), 201);
+	}
+
+	public function destroy(Request $request, $account_id = null)
+	{
+		$account = Account::whereRaw('user_id = ? and account_id = ?',
+					[$request->user_id, $account_id])->first();
+		if(!sizeof($account)){
+			return response()->json(['error' => 'Account does not exist'], 404);
+		}
+		Budget::where('account_id', $account_id)->delete();
+		return response()->json(['message' => 'Successful'], 200);
+	}
+
+}
