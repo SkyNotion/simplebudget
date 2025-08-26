f2688ab8e8c17f150334f73e1308e81d8d2e9bca
SkyNotion <meshdrew5@gmail.com>
2025-08-26 20:06:47 +0100

    fix: changed the way methods of the Responses class are accessed within the class from `$this->` to `self::`
    As they are static methods the cannot be accessed through calling an instance of the class ($this) with the object operator (->), But can only be accessed by calling the class (self) with the scope resolution operator (::).

--- app/Custom/Responses.php
+++ app/Custom/Responses.php
@@ -7,15 +7,15 @@ class Responses{
 	}
 
 	public static function message($message, $status_code = 200){
-		return $this->json(['message' => $message], $status_code);
+		return self::json(['message' => $message], $status_code);
 	}
 
 	public static function error($message, $status_code){
-		return $this->json(['error' => $message], $status_code);
+		return self::json(['error' => $message], $status_code);
 	}
 
 	public static function success(){
-		return $this->message('Successful', 200);
+		return self::message('Successful', 200);
 	}
 
 	public static function noContent(){
@@ -23,15 +23,15 @@ class Responses{
 	}
 
 	public static function noAccount(){
-		return $this->error('Account does not exist', 404);
+		return self::error('Account does not exist', 404);
 	}
 
 	public static function noParent(){
-		return $this->error('Parent account does not exists', 404);
+		return self::error('Parent account does not exists', 404);
 	}
 
 	public static function noChildAccounts(){
-		return $this->error('No child accounts found', 404);
+		return self::error('No child accounts found', 404);
 	}
 
 	public static function noBudget(){
@@ -39,14 +39,14 @@ class Responses{
 	}
 
 	public static function noRequestBody($message = 'Request body cannot be empty'){
-		return $this->error($message, 400);
+		return self::error($message, 400);
 	}
 
 	public static function noTransaction(){
-		return $this->error('Account or transaction does not exist', 404);
+		return self::error('Account or transaction does not exist', 404);
 	}
 
 	public static function apiKeyExists(){
-		return $this->error('Api key or name does not exist', 400);
+		return Responses::error('Api key or name does not exist', 400);
 	}
 }
